0396184186c4528c4e7ae343cf5a04b9d8a21f6f
hfolk25 <hfolk25@gmail.com>
2021-02-28 19:23:07 -0800

    adding routes

---
+++ routes/api/category-routes.js
@@ -0,0 +1,34 @@
+const router = require("express").Router();
+const { Category, Product } = require("../../models");
+
+// The `/api/categories` endpoint
+
+router.get("/", async (req, res) => {
+  // find all categories
+  // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findAll();
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  // find one category by its `id` value
+  // be sure to include its associated Products
+});
+
+router.post("/", async (req, res) => {
+  // create a new category
+});
+
+router.put("/:id", async (req, res) => {
+  // update a category by its `id` value
+});
+
+router.delete("/:id", async (req, res) => {
+  // delete a category by its `id` value
+});
+
+module.exports = router;
